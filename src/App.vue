f7511e13ac2fa60713ce3f8dba74349b24e83190
binarytrance <ganeshan.dash@gmail.com>
2023-03-13 01:22:22 +0530

    commit with config email change

--- src/App.vue
+++ src/App.vue
@@ -44,7 +44,7 @@ export default {
     <!-- <BaseCounter v-if="currentPage === 'BaseCounter'" />
     <TheWire v-else-if="currentPage === 'TheWire'" />
     <Users v-else /> -->
-    <p>commit</p>
+    <p>commit with email change</p>
     <component :is="currentPage" key="currentPage" />
   </div>
 </template>
