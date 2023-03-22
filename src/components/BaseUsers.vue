e96d9d84f0d62ea0df129a517e590a915551721c
binarytrance <ganeshan.dash@gmail.com>
2023-03-22 20:00:07 +0530

    script setup impelementation

--- src/components/BaseUsers.vue
+++ src/components/BaseUsers.vue
@@ -1,54 +1,44 @@
-<script>
-import { reactive } from "vue";
+<script setup>
+import { reactive, defineProps, defineEmits, ref, computed } from "vue";
 
-export default {
-  async setup() {
-    console.log("setup");
-    const state = reactive({
-      composableUsers: [],
-    });
+// https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0
 
-    const fetchUsers = async () => {
-      return await fetch("https://jsonplaceholder.typicode.com/users").then(
-        (res) => res.json()
-      );
-    };
-    state.composableUsers = await fetchUsers();
-    console.log("pp", state.composableUsers);
-    return {
-      composableUsers: state?.composableUsers || [],
-    };
+// const pokemon = ref([]);
+
+defineProps({
+  title: {
+    type: String,
+    default: "Setup",
   },
-  data() {
-    return {
-      users: [],
-    };
-  },
-  methods: {
-    async fetchUsers() {
-      this.users = await fetch(
-        "https://jsonplaceholder.typicode.com/users"
-      ).then((res) => res.json());
-    },
-  },
-  created() {
-    this.fetchUsers();
-  },
+});
+
+defineEmits(["fav-user"]);
+
+const state = reactive({
+  composableUsers: [],
+});
+
+const fetchUsers = async () => {
+  return await fetch("https://jsonplaceholder.typicode.com/users").then((res) =>
+    res.json()
+  );
 };
+state.composableUsers = await fetchUsers();
+console.log("pp", state.composableUsers);
 </script>
 
 <template>
-  asdf
-  <div>
+  <!-- asdf -->
+  <!-- <div>
     <h2>created</h2>
     <ul>
       <li v-for="user in users" :key="`user${user.id}`">{{ user.name }}</li>
     </ul>
-  </div>
+  </div> -->
   <div>
-    <h2>setup</h2>
+    <h2>{{ title }}</h2>
     <ul>
-      <li v-for="user in composableUsers" :key="`user${user.id}`">
+      <li v-for="user in state.composableUsers" :key="`user${user.id}`">
         {{ user.name }}
       </li>
     </ul>
